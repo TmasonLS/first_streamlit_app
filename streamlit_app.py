c8c891f2c2eef1cdfa3d93c53ad5b589a6c95207
TmasonLS <70733880+TmasonLS@users.noreply.github.com>
2023-11-12 07:27:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Fruit Picker
-streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index), ['Avocado', 'Strawberry'])
+streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
